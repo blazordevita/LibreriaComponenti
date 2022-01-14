d1530269585e0b840a66c5ae750d4fd1a6eb94db
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2022-01-14 12:32:01 +0100

    articolo componenti generici

--- LibreriaComponenti/LibreriaComponenti/Models/WeatherForecast.cs
+++ LibreriaComponenti/LibreriaComponenti/Models/WeatherForecast.cs
@@ -1,12 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibreriaComponenti.Models;
 
 public class WeatherForecast
 {
+    [DisplayFormat(DataFormatString = "{0:d}")]
     public DateTime Date { get; set; }
 
+    [Display(ShortName = "Temp. (C)")]
     public int TemperatureC { get; set; }
 
+    [Display(ShortName = "Temp. (F)")]
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
+    public string Summary { get; set; }
 }
